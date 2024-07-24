edfd07f39f6fc62085428b4406034f9f9d4a7b8c
Akash kumar <akash8825317696@gmail.com>
2024-07-24 23:43:24 +0530

    commit_24_july_2024_23:43_Lecture18_mid

---
+++ src/Arrays4/Lecture18.java
@@ -0,0 +1,77 @@
+package Arrays4;
+
+public class Lecture18{
+
+    static void printArray(int[] a){
+        for(int i = 0 ; i < a.length; i++){
+            System.out.print(a[i]+ " ");
+        }
+    }
+    void swapTwoNumbers(int a, int b){
+        System.out.println("Before Swapping a = " + a);
+        System.out.println("Before Swapping b = " + b);
+        a = a + b; // a = 15
+        b = a - b; // b = 15-5 = 10
+        a = a - b; // a = 15-10 = 5
+        System.out.println("\n"+"After Swapping a = " + a);
+        System.out.println("After Swapping b = " + b);
+    }
+
+    static int[] reverseOfArray(int[] arr){
+        int m = arr.length;
+//        int ans[]= new int[m];
+        int n = arr.length;
+        int temp = 0; //1
+//        {1,2,3,4,5,6}
+        for(int i = 0 ; i < m ; i++){
+            if(m%2 != 0){
+                if(n == m/2+1){
+                    return arr;
+                }
+                temp = arr[i];
+                arr[i] = arr[n-1];
+                arr[n-1] = temp;
+                n--;
+            }else {
+                if(i+1 > m/2){
+                    return arr;
+                }
+                temp = arr[i];
+                arr[i] = arr[n-1];
+                arr[n-1] = temp;
+                n--;
+            }
+
+        }
+        return arr;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+//                   4 5 2 3 1
+        int a = 10;
+        int b = 5;
+
+        Lecture18 lc18 = new Lecture18();
+//        lc18.swapTwoNumbers(a,b);
+
+
+//        int[] ans = lc18.reverseOfArray(arr);
+//        printArray(ans);
+
+//        System.out.println();
+//        System.out.println();
+
+        int[] reversedArray = new int[arr.length];
+//        for(int i = arr.length - 1; i >= 0 ; i--){
+//            reversedArray[4-i]= arr[i];
+//        }
+        int i = arr.length-1;
+        while (i>=0){
+            reversedArray[4-i] = arr[i--];
+        }
+        printArray(reversedArray);
+    }
+}
