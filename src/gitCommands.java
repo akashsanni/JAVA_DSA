8b74e9be7a18351654257d6cecf17210b9730199
Akash kumar <akash8825317696@gmail.com>
2024-07-26 00:25:46 +0530

    git commands

---
+++ src/gitCommands.java
@@ -0,0 +1,40 @@
+public class gitCommands {
+
+    /*
+
+    GIT Commands and Important notes:
+
+ls - list files
+mkdir filename- make directory / make new file
+cd - change directory
+ls -a => lists all the hidden files
+git status  status of all the files and folders which are not commited!
+git add .  adds all the changes to the git
+git add filename
+git commit - m "initial commit"
+git restore --staged filename.extension
+git log  all the history of the commits
+rm -rf filename.extension  rm -rf names.txt   to delete the file
+git reset f439787104137895873417   all the commits above of f439787104137895873417 will be removed!
+
+Stashing changes:
+git stash  => to keep the changes to the backstage and call the changes whenever needed, changes i dont want to commit it but also dont want to loose it.
+git stash pop  => to call the stashed changes back
+git stash clear  => clear those changes which were kept in the stash
+
+git remote add origin https://git-hub-repo-1.git
+   (remote - working with url) (adding) (origin - referring name for repo)
+git remote -v  => will show all the urls that are attached to particular folder
+git push origin master  => push the changes in the origin url and master branch.
+
+git branch feature  => creating a new branch called feature
+git checkout feature => head is pointing to the feature branch
+git checkout main  => head is pointing back to the main branch
+git merge feature  => feature branch is merged into main branch
+
+git clone https://akash-url.git   to download git project in the machine
+git remote add upstream https://manavi-url.git  =>from where you have forked repository is "upstream" url
+
+
+     */
+}
